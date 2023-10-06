5e7632a865c7f883fcf59b8c5f99a0b01191bc9f
tigrik3d <98457407+tigrik3d@users.noreply.github.com>
2023-10-06 14:38:08 +0300

    Add files via upload

--- realgymzp/js/swiper.js
+++ realgymzp/js/swiper.js
@@ -2,6 +2,7 @@ const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
+	// slidesPerView: 2,
  
 	// If we need pagination
 	pagination: {
@@ -19,4 +20,27 @@ const swiper = new Swiper('.swiper', {
 		delay:2000,
 	}
  
- });
+});
+
+
+// let sliderIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     let slides = document.querySelectorAll('.staff__column');
+    
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = 'none';
+//     }
+    
+//     sliderIndex++;
+    
+//     if (sliderIndex > slides.length) {
+//         sliderIndex = 1;
+//     }
+    
+//     slides[sliderIndex - 1].style.display = 'block';
+    
+//     setTimeout(showSlides, 3000); // Изменяйте время, чтобы настроить скорость смены слайдов
+// }
+
