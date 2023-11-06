159be0c216a435eea37cc7fd9466a7c76f25022c
tigrik3d <98457407+tigrik3d@users.noreply.github.com>
2023-11-06 15:14:55 +0200

    Add files via upload

--- realgymzp/js/swiper.js
+++ realgymzp/js/swiper.js
@@ -1,21 +1,21 @@
- const swiper = new Swiper('.slider1', {
-// 	// Optional parameters
+ const swiper1 = new Swiper('.swiper1', {
+// Optional parameters
 direction: 'horizontal',
  	loop: true,
-// 	// slidesPerView: 2,
+ 	// slidesPerView: 2,
  
-// 	// If we need pagination
+ 	// If we need pagination
  	pagination: {
  		el: '.swiper-pagination',
  		clickable:true,
 	 },
 	  
  
-// 	// Navigation arrows
-// 	// navigation: {
-// 	//   nextEl: '.swiper-button-next',
-// 	//   prevEl: '.swiper-button-prev',
-// 	// },
+// Navigation arrows
+ 	// navigation: {
+ 	//   nextEl: '.swiper-button-next',
+ 	//   prevEl: '.swiper-button-prev',
+ 	// },
 
  	autoplay: {
  		delay:2000,
@@ -23,59 +23,22 @@ direction: 'horizontal',
 	 
  
  });
-
- // Инициализация первого слайдера
-// var slider1 = new Swiper('.slider1', {
-// 	direction: 'horizontal',
-// 	loop: true,
-// 	slidesPerView: 2,
-// 	pagination: {
-// 	  el: '.swiper-pagination',
-// 	  clickable: true,
-// 	},
-// 	autoplay: {
-// 	  delay: 2000,
-// 	},
-//  });
  
- // Инициализация второго слайдера
- var slider2 = new Swiper('.slider2', {
-	direction: 'horizontal',
+ const swiper2 = new Swiper('.swiper2', {
+	// Опции вашего второго Swiper
+	 direction: 'horizontal',
+	 centeredSlides: true, // Центрирование активного слайда
+    spaceBetween: 5, // Расстояние между слайдами
 	loop: true,
+	slidesPerView: 1.6,
+	//  spaceBetween: 20,
+	//  initialSlide: 2,
+	//  centeredSlides: true,
 	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	 },
-	 centeredSlides: true, // Центрировать активный слайд
-	 spaceBetween: 14, // Убрать промежутки между слайдами
-
+		 el: '.swiper-pagination',
+		 clickable: true,
+	},
 	// autoplay: {
-	//   delay: 5000,
-	//  },
-	 
- });
- 
- // Создание медиа-запроса для второго слайдера
- const mediaQuery = window.matchMedia("(max-width: 760px)");
- 
- // Функция для обработки изменений состояния медиа-запроса
- function handleMediaChange(event) {
-	if (event.matches) {
-	  // Сработает, если ширина экрана <= 510px
-	  slider2.params.slidesPerView = 2;
-	  slider2.params.centeredSlides = true;
-	} else {
-	  // Сработает, если ширина экрана > 510px
-	  slider2.params.slidesPerView = 1;
-	  slider2.params.centeredSlides = false;
-	}
- 
-	// Обновление второго Swiper слайдера
-	slider2.update();
- }
- 
- // Добавление слушателя на изменения состояния медиа-запроса
- mediaQuery.addListener(handleMediaChange);
- 
- // Вызов функции обработки для начальной проверки
- handleMediaChange(mediaQuery);
+	// 	 delay: 2000,
+	// },
+});
